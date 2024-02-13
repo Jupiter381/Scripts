8f2681bddd357c46c2f7a051586fced662efb174
Vmax0 <155744672+Vmax0@users.noreply.github.com>
2024-02-13 10:57:49 +0100

    Update Main.lua

--- ProjectVMAX/Main.lua
+++ ProjectVMAX/Main.lua
@@ -1,15 +1,1 @@
---//Variables
 
-local games = {
-    [7346416636] = "https://raw.githubusercontent.com/Vmax0/Vmax0/main/ProjectVMAX/PopitHub.lua",
-    [7903991471] = "https://raw.githubusercontent.com/Vmax0/Vmax0/main/ProjectVMAX/PetTrading.lua"
-
-}
-
---//Main Code
-
-if games[game.PlaceId] ~= nil then
-	loadstring(game:HttpGet(games[game.PlaceId]))()
-else
-    print('1')
-end
