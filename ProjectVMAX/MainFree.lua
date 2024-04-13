468159cb185e3a514533f87219ea08a1c3a73495
Vmax0 <155744672+Vmax0@users.noreply.github.com>
2024-04-13 13:17:34 +0200

    Update MainFree.lua

--- ProjectVMAX/MainFree.lua
+++ ProjectVMAX/MainFree.lua
@@ -5,6 +5,9 @@ local ID = game.PlaceId
 if ID == 7346416636 then
    print("Pop It Trading free has been Loaded!")
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Vmax0/RobloxScripts/main/FreeScript/PopItTrading.lua"))()
+elseif ID == 13127800756 then
+  print("Arm Wrestle has been Loaded!")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Vmax0/RobloxScripts/main/FreeScript/ArmWrestle.lua"))()
   else
     print("Unknown PlaceId:", ID)
 end
