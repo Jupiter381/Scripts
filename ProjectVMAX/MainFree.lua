f8ec4274b5e4159e9f4fa25703ac09ef9cb26733
Vmax0 <155744672+Vmax0@users.noreply.github.com>
2024-04-13 12:47:03 +0200

    Update MainFree.lua

--- ProjectVMAX/MainFree.lua
+++ ProjectVMAX/MainFree.lua
@@ -3,7 +3,6 @@
 
 local ID = game.PlaceId
 if ID == 7346416636 then
-    warn("Pop It Trading Free has been Loaded!")
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Vmax0/RobloxScripts/main/FreeScript/PopItTrading.lua"))()
   else
     print("Unknown PlaceId:", ID)
