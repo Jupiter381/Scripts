09750eda9199adba6f47153c21fe44cf37296f08
Vmax0 <155744672+Vmax0@users.noreply.github.com>
2024-10-18 14:56:12 +0200

    Update MainFree.lua

--- ProjectVMAX/MainFree.lua
+++ ProjectVMAX/MainFree.lua
@@ -83,9 +83,6 @@ elseif ID == 18106735244 then
 elseif ID == 18213102218 then
   print("Titan Training Simulator has been Loaded!")
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Vmax0/RobloxScripts/main/FreeScript/TitantrainSim.lua"))()
-elseif ID == 18401171146 then
-  print("Anime Strike Simulator has been Loaded!")
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Vmax0/RobloxScripts/main/FreeScript/AnimeStrikeSimulator.lua"))()
 elseif ID == 15014439457 then
   print("Demon Blade has been Loaded!")
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Vmax0/RobloxScripts/main/FreeScript/DemonBlade.lua"))()
